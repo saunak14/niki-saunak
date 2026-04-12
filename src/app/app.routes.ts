7cd5332e16de8c6b0296be415318c81a2200bedd
Saunak Sahoo <saunak.x@gmail.com>
2026-04-11 21:46:24 -0700

    Added cards for widgets and implemented Snake game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'snake',
+    loadComponent: () => import('./games/snake/snake').then((m) => m.SnakeComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
